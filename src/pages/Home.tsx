496e16a108815a5d75d503afc142de56b9196926
Bentley Davis <bentley@bentleydavis.com>
2024-06-30 10:16:46 -0500

    remove unused signInwithRedirect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import '@/App.css';
 import { useEffect, useReducer, useState } from 'react';
 import { surveyFormat } from '@/surveyFormat';
 import { firebaseApp, firebaseInit } from '@/utils/database';
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signInWithRedirect } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 import { doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { newId } from '@/utils/newId';

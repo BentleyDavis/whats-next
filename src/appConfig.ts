871d083a2cb4b69d5dd82b22a6547217a4859076
Bentley Davis <bentley@bentleydavis.com>
2024-12-24 07:58:55 -0600

    v 0.0.10

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -3,7 +3,7 @@ const location = document.location.toString();
 let appConfig = {
     appName: `What's Next`,
     appId: `whatsnext`,
-    version: `0.0.9`,
+    version: `0.0.10`,
     environment: 'prod',
 
     hourToStartNewDay: 4,
